3c7ab73ce50cf991e231239a5363d89b03fa7e46
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2021-12-22 14:42:02 +0630

    first commit

---
+++ classEnroll.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="./assets/images/logo.jpg" />
+    <title>JKT Myanmar International - JAPANESE SCHOOL</title>
+    <meta
+      name="description"
+      content="We offer entry-level to business-level Japanese and we also offer conversation courses for those who wish to go to Japan for study or work purposes, or to work in a local Japanese company."
+    />
+    <meta
+      name="keywords"
+      content="jkt myanmar, jkt, JKT mm, JKT mm international, Business Counseling, IT, training, language school, Overseas Employment, JKT Myanmar International Co., Ltd., JKT Myanmar International"
+    />
+    <meta name="author" content="JKT IT Team" />
+    <meta name="title" content="JKT Myanmar International Co., Ltd." />
+    <meta name="copyright" content="JKT Myanmar International" />
+    <meta name="robots" content="japanese school, follow" />
+    <meta
+      name="googlebot"
+      content="jkt myanmar, jkt, JKT mm, JKT mm international, Business Counseling, IT, training, language school, Overseas Employment"
+    />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="revisit-after" content="1 days" />
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+    />
+
+    <!-- css -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/css/owl.carousel.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
+  </head>
+  <body>
+  <nav
+      class="navbar navbar-light navbar-expand-lg gray-dark float-panel"
+      data-top="0"
+      data-scroll="300"
+    >
+      <div class="container-fluid mynav">
+        <a href="index.html" class="navbar-brand">
+          <img
+            src="./assets/images/logo.jpg"
+            alt=""
+            height="50px"
+            width="50px"
+          />
+          <span style="font-weight: bolder; font-size: larger; color: #029eff"
+            >JKT</span
+          >
+          Myanmar International
+        </a>
+        <a href="index.html" class="small-brand">
+          <img
+            src="./assets/images/logo.jpg"
+            alt=""
+            height="50px"
+            width="50px"
+          />
+          <span style="font-weight: bolder; font-size: larger; color: #029eff"
+            >JKT</span
+          >
+          Myanmar International
+        </a>
+        <a href="index.html" class="icon-brand">
+          <img
+            src="./assets/images/logo.jpg"
+            alt=""
+            height="50px"
+            width="50px"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon toggler-icon-color"></span>
+        </button>
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+          <ul class="navbar-nav ms-auto text-sm-start">
+            <li class="nav-item">
+              <a href="index.html" class="nav-link active"> HOME </a>
+            </li>
+            <li class="nav-item">
+              <a href="./about.html" class="nav-link active"> ABOUT </a>
+            </li>
+            <li class="nav-item">
+              <a href="./activities.html" class="nav-link active">
+                ACTIVITIES
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                href="./services.html"
+                class="nav-link active"
+                id="serviceNavbarDropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                SERVICES <i class="fas fa-angle-down"></i>
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="serviceNavbarDropdown"
+              >
+                <a class="dropdown-item" href="./overseas.html"
+                  >OVERSEAS EMPLOYMENT</a
+                >
+                <a class="dropdown-item" href="./business.html"
+                  >BUSINESS CONSULTANT</a
+                >
+                <a class="dropdown-item" href="./announcement.html"
+                  >IT SERVICES</a
+                >
+                <a class="dropdown-item" href="./announcement.html"
+                  >TRAVEL AND TOULS</a
+                >
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                href="./trainings.html"
+                class="nav-link"
+                id="trainingNavbarDropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                TRAININGS <i class="fas fa-angle-down"></i>
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="trainingNavbarDropdown"
+              >
+                <a class="dropdown-item" href="./jp-school.html"
+                  >JAPANESE LANGUAGE SCHOOL</a
+                >
+                <a class="dropdown-item" href="./announcement.html"
+                  >VOCATIONAL TRAINING</a
+                >
+                <a class="dropdown-item" href="./announcement.html"
+                  >HR TRAINING</a
+                >
+              </div>
+            </li>
+            <li class="nav-item">
+              <a href="./contact.html" class="nav-link active"> CONTACT </a>
+            </li>
+            <li class="lang">
+              <div class="btn-group" role="group" aria-label="First group">
+                <a href="./jp-school.html"
+                  ><button
+                    type="button"
+                    class="btn btn1"
+                    style="background-color: rgba(91, 175, 231, 0.5)"
+                  >
+                    <img
+                      src="./assets/images/icon/ukFlag.png"
+                      height="20px"
+                      width="25px"
+                    /></button
+                ></a>
+                <a href="./mm/jp-school.html"
+                  ><button
+                    type="button"
+                    class="btn btn2"
+                  >
+                    <img
+                      src="./assets/images/icon/mmFlag.svg"
+                      height="20px"
+                      width="25px"
+                    /></button
+                ></a>
+                <a href="./jp/jp-school.html"
+                  ><button
+                    type="button"
+                    class="btn btn3"
+                  >
+                    <img
+                      src="./assets/images/icon/japanFlag.jpg"
+                      height="20px"
+                      width="25px"
+                    /></button
+                ></a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="btn-group lang-xl" role="group" aria-label="First group">
+          <a href="./jp-school.html"
+            ><button
+              type="button"
+              class="btn btn1"
+              style="background-color: rgba(91, 175, 231, 0.5)"
+            >
+              <img
+                src="./assets/images/icon/ukFlag.png"
+                height="20px"
+                width="25px"
+              /></button
+          ></a>
+          <a href="./mm/jp-school.html"
+            ><button
+              type="button"
+              class="btn btn2"
+            >
+              <img
+                src="./assets/images/icon/mmFlag.svg"
+                height="20px"
+                width="25px"
+              /></button
+          ></a>
+          <a href="./jp/jp-school.html"
+            ><button
+              type="button"
+              class="btn btn3"
+            >
+              <img
+                src="./assets/images/icon/japanFlag.jpg"
+                height="20px"
+                width="25px"
+              /></button
+          ></a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- JP School header start -->
+    <section>
+      <div class="header">
+        <h3>Japanese Language School - Enrollment Form</h3>
+        <div class="bg-cover"></div>
+        <img src="./assets/images/cover/cover.jpg" alt="jpschool-cover" />
+      </div>
+    </section>
+    <!-- JP School header end -->
+  </body>
+<html>
