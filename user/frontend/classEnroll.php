67f3085da1a482b5d75217f47075b5963c79b297
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-12-27 11:26:44 +0630

    jquery not working on server fixed

--- user/frontend/classEnroll.php
+++ user/frontend/classEnroll.php
@@ -415,8 +415,8 @@
 
   <!-- script -->
   <script src="./assets/js/jquery-3.6.0.js"></script>
-  <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.js"></script>
-  <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.js"></script>
+  <script type="text/javascript" src="./assets/js/validation.js"></script>
+  <script type="text/javascript" src="./assets/js/additional-methods"></script>
   <script src="./assets/js/popper.min.js"></script>
   <script src="./assets/js/bootstrap.min.js"></script>
   <script src="./assets/js/float-panel.js"></script>
