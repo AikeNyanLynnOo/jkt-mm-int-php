7cfe034a36bb4c21b0a1a4c3ae79749ee32bd403
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-12-29 10:23:56 +0630

    confirm alert before submit still not going successful stage

--- user/frontend/classEnroll.php
+++ user/frontend/classEnroll.php
@@ -26,8 +26,6 @@
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css'>
 
   <!-- css -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
   <link rel="stylesheet" href="./assets/css/owl.carousel.css" />
   <link rel="stylesheet" href="./assets/css/main.css" />
@@ -227,13 +225,13 @@
                   <div class="row mb-3">
                     <div class="col-12 col-sm-12 col-md-12">
                       <label class="fieldlabels">Phone Number: <span class="required-tag">required &nbsp; *</span></label>
-                      <input type="text" class="form-input" name="phone" id="phone" placeholder="09XXXXXXXXX" />
+                      <input type="text" class="form-input" name="phone" id="phone" placeholder="09989898989" />
                     </div>
                   </div>
                   <div class="row mb-3">
                     <div class="col-12 col-sm-12 col-md-12">
                       <label class="fieldlabels">Address: <span class="required-tag">required &nbsp; *</span></label>
-                      <textarea name="address" class="form-input" id="address" placeholder="e.g. No.(X), XXX Road, XXX."></textarea>
+                      <textarea name="address" class="form-input" id="address" placeholder="e.g. No.(), (---) Road, (---) City."></textarea>
                     </div>
                   </div>
                   <div class="row mb-3">
@@ -466,8 +464,8 @@
 
   <!-- script -->
   <script src="./assets/js/jquery-3.6.0.js"></script>
-  <script type="text/javascript" src="./assets/js/validation.js"></script>
-  <script type="text/javascript" src="./assets/js/additional-methods"></script>
+  <script src="./assets/js/validation.js"></script>
+  <script src="./assets/js/additional-methods"></script>
   <script src="./assets/js/popper.min.js"></script>
   <script src="./assets/js/bootstrap.min.js"></script>
   <script src="./assets/js/float-panel.js"></script>
