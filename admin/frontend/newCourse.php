54bc4aaad89964edbca9e2d01ff8772df05e350e
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2022-01-10 15:49:57 +0630

    change2

--- admin/frontend/newCourse.php
+++ admin/frontend/newCourse.php
@@ -443,7 +443,6 @@ include("../confs/config.php");
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
-    <script src="js/checkboxValidate.js"></script>
 </body>
 
 </html>
