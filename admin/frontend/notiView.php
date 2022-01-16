b091da8349164fd6f2ee670aa93b37c9b362deca
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2022-01-16 22:57:23 +0630

    remove email icon and add notification right and clean files

--- admin/frontend/notiView.php
+++ admin/frontend/notiView.php
@@ -1,5 +1,21 @@
 <?php
-// check user
+session_start();
+include_once '../auth/authenticate.php';
+include "../confs/config.php";
+$get_notifications = "SELECT * FROM notifications WHERE seen=0 AND created_at >= DATE_SUB(NOW(),INTERVAL 6 HOUR)";
+$all_result = mysqli_query($conn, $get_notifications);
+
+if (!isset($_GET["id"])) {
+    $get_all = "SELECT * FROM notifications";
+    $get_result = mysqli_query($conn, $get_all);
+} else {
+    $noti_id = $_GET["id"];
+    $get_noti = "SELECT * FROM notifications where noti_id = $noti_id";
+    $get_result = mysqli_query($conn, $get_noti);
+    $set_seen_noti = "UPDATE notifications set seen=1 WHERE noti_id = $noti_id";
+    mysqli_query($conn, $set_seen_noti);
+}
+
 ?>
 <html lang="en">
 
@@ -11,14 +27,16 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>JKTMyanmar Admin</title>
+    <link rel="shortcut icon" href="img/logo.jpg" />
+    <title>JKT Admin</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
 
@@ -31,7 +49,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -43,7 +61,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -54,13 +72,19 @@
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="fas fa-fw fa-user"></i>
                     <span>Enrollments</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Operations</h6>
-                        <a class="collapse-item" href="buttons.html">New Enrollment</a>
+                        <a class="collapse-item" href="newEnroll.php">
+                            <i class="fas fa-fw fa-user-plus"></i>
+                            <span>New Enrollment</span>
+                        </a>
+                        <a class="collapse-item" href="enrollments.php">
+                            <i class="fas fa-fw fa-paperclip"></i>
+                            <span>All Enrollments</span>
+                        </a>
                     </div>
                 </div>
             </li>
@@ -68,13 +92,30 @@
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
+                    <i class="fas fa-fw fa-graduation-cap"></i>
                     <span>Courses</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="utilities-color.html">Categories</a>
-                        <a class="collapse-item" href="utilities-border.html">Types</a>
+                        <a class="collapse-item" href="newCourse.php">
+                            <i class="fas fa-fw fa-folder-plus"></i>
+                            <span>New Course</span>
+                        </a>
+                        <a class="collapse-item" href="courses.php">
+
+                            <i class="fas fa-fw fa-folder-open"></i>
+                            <span>All Courses</span>
+                        </a>
+                        <a class="collapse-item" href="categories.php">
+                            <i class="fas fa-fw fa-clipboard-list"></i>
+                            <span>Categories</span>
+
+                        </a>
+                        <a class="collapse-item" href="types.php">
+
+                            <i class="fas fa-fw fa-thumbtack"></i>
+                            <span>Course types</span>
+                        </a>
                     </div>
                 </div>
             </li>
@@ -120,87 +161,47 @@
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search nav-title">
+                        <h3>Dashboard</h3>
+                    </div>
+
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
 
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
                                 <!-- Counter - Alerts -->
-                                <span class="badge badge-danger badge-counter">3+</span>
+                                <span class="badge badge-danger badge-counter"><?php echo mysqli_num_rows($all_result) ?>+</span>
                             </a>
                             <!-- Dropdown - Alerts -->
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
                                 <h6 class="dropdown-header">
-                                    Alerts Center
+                                    Notifications Center
                                 </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-file-alt text-white"></i>
+                                <?php while ($row = mysqli_fetch_assoc($all_result)) : ?>
+                                    <a class="dropdown-item d-flex align-items-center" href="notiView.php?id=<?php echo $row['noti_id'] ?>">
+                                        <div class="mr-3">
+                                            <div class="icon-circle bg-primary">
+                                                <?php if ($row["type"] == "PENDING_REQUEST") : ?>
+                                                    <i class="fas fa-user-plus text-white"></i>
+                                                <?php elseif ($row["type"] == "PAYMENT") : ?>
+                                                    <i class="fas fa-donate text-white"></i>
+                                                <?php else :  ?>
+                                                    <i class="fas fa-exclamation-triangle text-white"></i>
+                                                <?php endif; ?>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
+                                        <div>
+                                            <div class="small text-gray-500"><?php echo $row["created_at"] ?></div>
+                                            <span class="font-weight-bold"><?php echo $row["title"] ?></span>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                                    </a>
+                                <?php endwhile; ?>
+
+                                <a class="dropdown-item text-center small text-gray-500" href="notiView.php">Show All Alerts</a>
                             </div>
                         </li>
 
@@ -269,7 +270,7 @@
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $name; ?></span>
                                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
@@ -300,22 +301,40 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Courses</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-                    </div>
-
-                    <!-- Content Row -->
-                    <div class="row">
-
-                        <!-- courses table -->
-                    </div>
+                <div class="container">
+                    <?php while ($noti = mysqli_fetch_assoc($get_result)) : ?>
+                        <ul class="list-unstyled col-12 col-md-9 mx-auto card py-4 px-5">
+                            <li class="row justify-content-between border-bottom py-2">
+                                <span class="font-weight-bold">Title</span>
+                                <span><?php echo $noti["title"] ?></span>
+                            </li>
+                            <li class="row justify-content-between border-bottom py-2">
+                                <span class="font-weight-bold">Description</span>
+                                <span><?php echo $noti["description"] ?></span>
+                            </li>
+                            <li class="row justify-content-between border-bottom py-2">
+                                <span class="font-weight-bold">Type</span>
+                                <span><?php echo $noti["type"] ?></span>
+                            </li>
+                            <?php
+                            if ($noti["enrollment_id"]) :
+                            ?>
+                                <li class="row justify-content-between py-2">
+                                    <span class="font-weight-bold">Enrollment ID</span>
+                                    <span><?php echo $noti["enrollment_id"] ?></span>
+                                </li>
+                            <?php else : ?>
+                                <li class="row justify-content-between py-2">
+                                    <span class="font-weight-bold">Payment ID</span>
+                                    <span><?php echo $noti["payment_id"] ?></span>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    <?php endwhile; ?>
 
                 </div>
                 <!-- /.container-fluid -->
+
 
             </div>
             <!-- End of Main Content -->
@@ -354,7 +373,7 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="../auth/logoutBackend.php">Logout</a>
                 </div>
             </div>
         </div>
@@ -369,6 +388,9 @@
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+    <!-- <script src="js/jquery.min.js"></script> -->
+    <script src="js/Chart.min.js"></script>
+    <script src="js/myChart.js"></script>
 </body>
 
 </html>
